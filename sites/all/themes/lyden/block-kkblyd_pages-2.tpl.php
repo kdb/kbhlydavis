6c9b52f3c3d0184ded73ee8a58cdebd1fefdca5b
Jakob Torp Svendsen <jakob@revealit.dk>
2009-10-31 21:10:56 +0100

    Front page styles.

--- sites/all/themes/lyden/block-kkblyd_pages-2.tpl.php
+++ sites/all/themes/lyden/block-kkblyd_pages-2.tpl.php
@@ -28,7 +28,9 @@
 ?>
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?> clear-block">
   <div class="main">
+    <div class="papers">
     <span class="papers"></span>
+    </div>
     <div class="wrapper">
       <?php if ($block->subject): ?>
         <h2><?php print $block->subject ?></h2>
