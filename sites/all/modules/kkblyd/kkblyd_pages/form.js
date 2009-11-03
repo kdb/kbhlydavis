d15925cd8cc9a211be4cfcfba6bd51c83314e7da
Jakob Torp Svendsen <jakob@revealit.dk>
2009-11-03 10:26:58 +0100

    Made icons clickable

--- sites/all/modules/kkblyd/kkblyd_pages/form.js
+++ sites/all/modules/kkblyd/kkblyd_pages/form.js
@@ -6,4 +6,7 @@ $(document).ready(function() {
   $("#edit-download").click(function() {
     setTimeout('Drupal.settings.kkblyd_frontpage()', 500);
   });
+  $("form#kkblud-pages-get-table-form .form-wrapper-wrapper span.download").click(function() {
+    $("#edit-download").click();
+  });
 });
